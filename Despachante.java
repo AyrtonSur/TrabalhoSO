313b9d8c7d91b5816f9a74fd78dd31938fe82e1b
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-22 18:24:31 -0300

    fix: consertando a identação

--- Despachante.java
+++ Despachante.java
@@ -7,35 +7,35 @@ class Despachante implements Runnable {
   private static final int QUANTUM = 4;
 
   public Despachante(Fila<Processo> prontos, Fila<Processo> auxiliar, CPU[] cpus) {
-      this.filaProntos = prontos;
-      this.filaAuxiliar = auxiliar;
-      this.cpus = cpus;
+    this.filaProntos = prontos;
+    this.filaAuxiliar = auxiliar;
+    this.cpus = cpus;
   }
 
   @Override
   public void run() {
-      while (true) {
-          for (CPU cpu : cpus) {
-              while (cpu.getProcesso() == null) { // Enquanto cpu estiver livre loop irá se repetir até que um processo seja alocado para CPU
-                if ((Processo processo = filaAuxiliar.remover()) != null) { // verifica se existe um processo na filaAuxiliar dando preferência para a fila
-                  processo.setTransicaoDeEstados("executando"); // processo passa para estado executando
-                  cpu.execute(processo, QUANTUM); // processo é entregue para a cpu para começar a executar
-                } else { // para o caso em que não tinha processos na filaAuxiliar
-                  Processo processo = filaProntos.remover(); // pega o processo da fila de prontos
-                  if (processo != null) { // tem processo na fila de prontos
-                    processo.setTransicaoDeEstados("executando");
-                    cpu.execute(processo, QUANTUM);
-                  }
-                }
-              }
+    while (true) {
+      for (CPU cpu : cpus) {
+        while (cpu.getProcesso() == null) { // Enquanto cpu estiver livre loop irá se repetir até que um processo seja alocado para CPU
+          if ((Processo processo = filaAuxiliar.remover()) != null) { // verifica se existe um processo na filaAuxiliar dando preferência para a fila
+            processo.setTransicaoDeEstados("executando"); // processo passa para estado executando
+            cpu.execute(processo, QUANTUM); // processo é entregue para a cpu para começar a executar
+          } else { // para o caso em que não tinha processos na filaAuxiliar
+            Processo processo = filaProntos.remover(); // pega o processo da fila de prontos
+            if (processo != null) { // tem processo na fila de prontos
+              processo.setTransicaoDeEstados("executando");
+              cpu.execute(processo, QUANTUM);
+            }
           }
-
-          try {
-              TimeUnit.MILLISECONDS.sleep(50); // Simula controle periódico
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-              return;
-          }
+        }
       }
+
+      try {
+        TimeUnit.MILLISECONDS.sleep(50); // Simula controle periódico
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        return;
+      }
+    }
   }
 }
