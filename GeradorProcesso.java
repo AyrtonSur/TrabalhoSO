c8d683ceb07707a10e8b6704fdf37281d724a546
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-12-23 00:39:13 -0300

    Concepção inicial do gerador de processos

---
+++ GeradorProcesso.java
@@ -0,0 +1,122 @@
+import java.lang.Runnable;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class GeradorProcesso implements Runnable {
+    private static final String MENSAGEMERRO = "Erro: Informe um valor positivo maior que zero.";
+    
+    private Deque<Processo> filaProcessos;
+
+    private Fila<Descritor> filaProntos;
+    private MemoriaPrincipal memoriaP;
+
+
+    public GeradorProcesso(Fila<Descritor> filaProntos, MemoriaPrincipal memoriaP) {
+        this.filaProcessos = new ArrayDeque<Processo>();
+        this.filaProntos = filaProntos;
+        this.memoriaP = memoriaP;
+    }
+
+    private void adicionarProcesso(Processo p) {
+        synchronized(this.filaProcessos) {
+            this.filaProcessos.addLast(p);
+        }
+    }
+
+    private void processarProcessos() {
+        if (this.filaProcessos.peekFirst()) {
+            Processo processoAtual;
+            synchronized (this.filaProcessos) {
+                processoAtual = this.filaProcessos.pollFirst();
+            }
+            this.filaProntos.adicionar(processoAtual.getDescritor());
+            this.memoriaP.enviarProcesso(processoAtual);
+        }
+    }
+
+    public void gerarProcesso(Fila<Processo> filaProntos) {
+        /* Scanner para obter as informações dos novos processos */
+        Scanner scanIN = new Scanner(System.in);
+        
+        int tempoCPU1, tempoES, tempoCPU2, qtdMemoria;
+
+        System.out.println("---------------------------------");
+        System.out.println("Interface de Criação de Processos");
+        
+        /* Loop para obtenção do primeiro tempo de cpu */
+        while (true) {
+            try {
+                System.out.printf("Informe o primeiro tempo de CPU: ");
+                tempoCPU1 = scanIN.nextInt();
+                if (tempoCPU1 <= 0) {
+                    throw new InputMismatchException(GeradorProcesso.MENSAGEMERRO);
+                }
+                break;
+            } catch(InputMismatchException error) {
+                System.out.println(error);
+                continue;
+            }
+        }
+
+        /* Loop para obtenção do tempo de ES */
+        while (true) {
+            try {
+                System.out.printf("Informe o tempo de ES: ");
+                tempoES = scanIN.nextInt();
+                if (tempoES < 0) {
+                    throw new InputMismatchException(GeradorProcesso.MENSAGEMERRO);
+                }
+                break;
+            } catch(InputMismatchException error) {
+                System.out.println(error);
+                continue;
+            }
+        }
+
+        /* Loop para obtenção do segundo tempo de cpu */
+        while (true) {
+            try {
+                System.out.printf("Informe o segundo tempo de cpu: ");
+                tempoCPU2 = scanIN.nextInt();
+                if (tempoCPU2 < 0) {
+                    throw new InputMismatchException(GeradorProcesso.MENSAGEMERRO);
+                }
+                break;
+            } catch(InputMismatchException error) {
+                System.out.println(error);
+                continue;
+            }
+        }
+        
+        /* Loop para obtenção da quantidade de memória */
+        while (true) {
+            try {
+                System.out.printf("Informe a quantidade de memória em MB do processo: ");
+                qtdMemoria = scanIN.nextInt();
+                if (qtdMemoria < 0) {
+                    throw new InputMismatchException(GeradorProcesso.MENSAGEMERRO);
+                }
+                break;
+            } catch(InputMismatchException error) {
+                System.out.println(error);
+                continue;
+            }
+        }
+        
+        // TODO: retornar booleano ao alocar memória na mp? Contadição entre alocar memoria e criar processo
+
+        Processo novoProcesso = new Processo(qtdMemoria);
+        novoProcesso.setFaseAtual("Novo");
+        novoProcesso.setTempoFaseCpu1(tempoCPU1);
+        novoProcesso.setTempoDuracaoEntradaSaida(tempoES);
+        novoProcesso.setTempoFaseCpu2(tempoCPU2);
+
+        this.adicionarProcesso(novoProcesso);
+    }
+
+    public void run() {
+        while (true) { this.processarProcessos(); }
+    }
+}
