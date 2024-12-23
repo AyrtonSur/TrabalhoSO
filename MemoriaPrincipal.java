832816cb0403088f75a9af77e6c0ca941ada4153
carlosjr04 <carlosjr0421@gmail.com>
2024-12-23 15:12:35 -0300

    Nova Memoria principal
    Nela eu fiz um array de bytes(mais leve) com tamanho de 32000 elementos para determinar se tal local foi alocado ou nao( nao alocado =0 e alocado = 1). para alocar ele ve qual lugar tem a mesma quantidade de espaço vazio e tamanho do processo, ele tranforma essa area em 1, adiciona o processo em uma collection e cria uma map de id com lista de int(que reporesenta o começo do vetor e final de onde esta alocado o processo no array de bytes). e para desalocar ele ve no map onde começa e termina a alocação, verifica se nao é null para nao dar erro se nao existir tal processo alocado, bota tudo em 0, tira processo da collection e do map.

--- MemoriaPrincipal.java
+++ MemoriaPrincipal.java
@@ -1,50 +1,91 @@
 
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
 
-class MemoriaPrincipal {
+
+public class MemoriaPrincipal {
     private final int memoriaTotal = 32000;
-    private int memoriaDisponivel;
-    private Map<Integer,Integer> alocacao; //considerando que id é int porque imagino que vai ser gerado por incremento 
-    // o ideal é fazer um map de processo com int, e fazer os metodos com objeto processo tbm, porem como nao tem processo ainda ele vai ficar dando pal
-    public MemoriaPrincipal(){
-        this.memoriaDisponivel = memoriaTotal;
-        this.alocacao = new HashMap<>();
+    
+    private Collection<Processo> processos;
+    private HashMap<String, int[]> menorEmaior;
+    private byte[] alocacao;
+    public MemoriaPrincipal(){      
+        this.processos = new ArrayList<>();
+        this.alocacao = new byte[32000];
+        this.menorEmaior = new HashMap<>();
+        
     }
 
-    public int getMemoriaDisponivel() {
-        return memoriaDisponivel;
-    }
-
-    public Map<Integer, Integer> getAlocacao() {
-        return alocacao;
-    }
-
-    public synchronized void alocarMemoria(int idProcesso, int tamanho) {
-        if (tamanho <= memoriaDisponivel) {
-            alocacao.put(idProcesso, tamanho);
-            memoriaDisponivel -= tamanho;       
-            System.out.println("Memoria alocada");
+    
+    public synchronized void alocarMemoria(Processo processo, int tamanho) {
+        boolean alocado = false;
+        int countAlocacao = 0;
+        int espacolivre = 0;
+        while (alocado==false && countAlocacao<32000){
+            if (alocacao[countAlocacao]== (byte) 0){
+                espacolivre +=1;
+            }
+            if (alocacao[countAlocacao]== (byte) 1){
+                espacolivre = 0;
+            }
+            if (espacolivre==tamanho){
+                for(int i=(countAlocacao+1)-tamanho;i<=countAlocacao;i++){
+                    alocacao[i] = 1;
+                }
+                menorEmaior.put(processo.getId(), new int[]{(countAlocacao+1)-tamanho, countAlocacao+1});
+                processos.add(processo);
+                alocado = true;
+            }
+            countAlocacao+=1;
         }
-        System.out.println("Sem espaço na memoria para alocação");
+
+        if (alocado==true) {      
+            int[] valores = menorEmaior.get(processo.getId());
+            System.out.println("Memoria alocada para o processo"+ processo.getId() + "no espaço de" + valores[0] + "-" + valores[1]);
+        }
+        else{
+            System.out.println("Não foi possível alocar a memória");
+        }
     }
     
-    public synchronized  void liberarmemoria(int idProcesso){
-        if (alocacao.containsKey(idProcesso)){
-            int tamanho = alocacao.get(idProcesso);
-            alocacao.remove(idProcesso);
-            memoriaDisponivel += tamanho;
+    public synchronized  void liberarmemoria(Processo processo){
+        int[] valores = menorEmaior.get(processo.getid());
+        if (valores!=null){
+            int inicio = valores[0];
+            int finall = valores[1];
+
+            for (int i = inicio; i<=finall ;i++){
+                alocacao[i]=0;
+            }
+
+            menorEmaior.remove(processo.getId());
+            processos.remove(processo);
+            System.out.println("Memoria desalocada");
         }
+        else{
+            System.out.println("Este processo não foi alocado anteriormente");
+        }
+        
+        
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Memoria Principal:");
-        sb.append("memoriaTotal=").append(memoriaTotal);
-        sb.append(", memoriaDisponivel=").append(memoriaDisponivel);
-        sb.append(", alocacao=").append(alocacao);
+        sb.append("Memoria Total=").append(memoriaTotal);
+        sb.append(", Processos alocados=").append(processos);
         
         return sb.toString();
+    }
+
+    public int getMemoriaTotal() {
+        return memoriaTotal;
+    }
+
+    public Collection<Processo> getProcessos() {
+        return processos;
     }
 }
