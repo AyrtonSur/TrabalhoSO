3fa5dcc2f97c6635205b0aa03cc35950a5b55d9b
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-22 21:05:12 -0300

    fix: fazendo alterações para descritor em ES também

--- DispositivoES.java
+++ DispositivoES.java
@@ -1,33 +1,33 @@
 import java.util.concurrent.TimeUnit;
 
 class DispositivoES implements Runnable {
-  private final Fila<Processo> filaAuxiliar; // Fila que os processos seram enviados para
-  private final Processo processo;
+  private final Fila<Descritor> filaAuxiliar; // Fila que os Descritors seram enviados para
+  private final Descritor descritor;
 
-  public DispositivoES(Processo processo, Fila<Processo> auxiliar) {
-    this.processo = processo;
+  public DispositivoES(Descritor descritor, Fila<Descritor> auxiliar) {
+    this.descritor = descritor;
     this.filaAuxiliar = auxiliar;
   }
 
   @Override
   public void run() {
-    if (processo != null) {
-        System.out.printf("Processo #%d iniciou operação de E/S por %d u.t.%n",
-                processo.getId(), processo.getTempoDuracaoEntradaSaida());
+    if (descritor != null) {
+        System.out.printf("Descritor #%d iniciou operação de E/S por %d u.t.%n",
+                descritor.getId(), descritor.getTempoDuracaoEntradaSaida());
 
         // Simula o tempo de E/S
         try {
-            TimeUnit.MILLISECONDS.sleep(processo.getTempoDuracaoEntradaSaida() * 100); // Multiplica para ajustar unidades de tempo
+            TimeUnit.MILLISECONDS.sleep(descritor.getTempoDuracaoEntradaSaida() * 100); // Multiplica para ajustar unidades de tempo
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             return;
         }
 
-        System.out.printf("Processo #%d finalizou operação de E/S.%n", processo.getId());
+        System.out.printf("Descritor #%d finalizou operação de E/S.%n", descritor.getId());
 
-        // Marca o processo como "pronto" e move para a fila de prontos
-        processo.setTransicaoDeEstados("pronto");
-        filaAuxiliar.adicionar(processo);
+        // Marca o Descritor como "pronto" e move para a fila de prontos
+        descritor.setTransicaoDeEstados("pronto");
+        filaAuxiliar.adicionar(descritor);
     } else {
         // Aguarda brevemente antes de verificar novamente a fila de bloqueados
         try {
